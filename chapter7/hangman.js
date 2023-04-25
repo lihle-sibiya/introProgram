71c38e84deb429ff473579f91835dd491d4bb504
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-04-25 20:03:22 +0200

    Add files via upload

---
+++ chapter7/hangman.js
@@ -0,0 +1,55 @@
+let words = ["mill", "eat", "great", "food"];
+let word = words[Math.floor(Math.random() * words.length)];
+//Preparing empty answer array with underscores
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+//Remaing letters
+let remainingLetters = word.length;
+let numberOfGuesses = 6;
+let userName = prompt("What is your name?");
+alert("Welcome to HANGMAN " + userName);
+
+//Starting the game while loop
+while (remainingLetters > 0 && numberOfGuesses > 0) {
+    alert(answerArray.join(" "));
+
+    //Player's input
+    let guess = prompt("Guess a letter, or click Cancel to stop playing.");
+    numberOfGuesses--;
+    if (guess === null) {
+        break;
+
+    } else if (guess.length !== 1) {
+        alert("Guess a single letter.");
+    }
+    else {
+        //Check if the guess is correct
+        guess = guess.toLowerCase();
+        for (let j = 0; j < word.length; j++) {
+            if (word[j] === guess) {
+                if (answerArray[j] === "_") {
+                    answerArray[j] = guess;
+                    remainingLetters--;
+                }
+                else {
+                    alert("You have already guessed this letter.")
+                }
+            }
+        }
+    }
+
+}
+//Ending the game
+if (remainingLetters === 0) {
+    alert(answerArray.join(" "));
+    alert("CONGRATULATIONS " + userName + "!" + " You did a great job !!!")
+}
+else if (numberOfGuesses === 0) {
+    alert("You have ran out of guesses " + userName + "!" + " The answer is " + word);
+}
+else {
+    alert("You quit the game. " + "Nicely tried! " + userName + "!!!" +" The answer is " + word);
+}
+
