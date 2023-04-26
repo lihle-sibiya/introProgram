96fa52c8abac22c66e861c5eb10dad1344f70f1f
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-04-26 15:25:25 +0200

    Add files via upload

---
+++ Book2 chapter1/Book2chapter1.js
@@ -0,0 +1,17 @@
+//Javascript commands
+console.log(2000 - 37);
+console.log(30 * 27);
+console.log(120 /20);
+console.log("Lihle" + " " + "is learning JavaScript!");
+//Returns error message when there is syntax error
+//console.log("Lihle" + + is learning JavaScript!);
+
+//Having Fun with Maths
+console.log(0.5 * 1.5);
+console.log(1 + 1 * 4 / 8);
+console.log(5);
+console.log("Number " + 5);
+console.log( 5 + " number");
+console.log("Lihle" +  " " + "Ntokozo Mbambo");
+console.log(1200009 + 4098920019);
+console.log(0.08847919388 + 0.881733);
