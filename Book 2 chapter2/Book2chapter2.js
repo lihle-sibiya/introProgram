d51021745911497f15425e4ef2afc358bea4b609
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-04-26 16:08:25 +0200

    Add files via upload

---
+++ Book 2 chapter2/Book2chapter2.js
@@ -0,0 +1,13 @@
+//Javascript statement
+console.log("Coding is fun!");
+
+//Listing 2-2 A Program to Print a Message 10 Times
+for (let i = 0; i < 10; i++) {
+    document.write("Joe said, \"Hi!\"");
+}
+
+//escaping the quotation mark
+console.log("Joe said, \"Hi!\"");
+
+//Comments: one line//
+/*This is how you write a multi-line comment*/
