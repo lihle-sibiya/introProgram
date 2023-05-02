bae839fd055e08146dd9d914936c30e2e4080828
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-05-02 22:47:15 +0200

    Add files via upload

---
+++ Book 2 chapter11/Book2chapter11.js
@@ -0,0 +1,47 @@
+let people = ["Mary", "bobby", "Judy", "Eddie", "Herbie", "Bobby"];
+let otherPeople = ["Teddy", "Cathy", "G-Berg", "Georgie"];
+
+//document.getElementById("peopleIKnow").innerHTML = people.toString();
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+
+//allPeople = people.concat(otherPeople);
+
+//people = people.indexOf("Eddie");
+
+//people = people.join(" # ");
+
+//people = people.lastIndexOf("Bobby");
+
+//people = people.pop();
+
+//people.pop();
+
+
+//people = people.push("Teddy");
+
+//people.push("Teddy");
+
+//people = people.reverse();
+
+//people.shift();
+
+//people.unshift("Teddy");
+
+
+//people = people.slice(0,3);
+
+//people = people.sort();
+
+//people.splice(1,0,"Cathy");
+document.getElementById("peopleIKnow").innerHTML = people;
+
+//people.unshift("Teddy");
+//people = people.slice(0,3);
+//people = people.sort();
+//people.splice(1,0,"Cathy"); //Mary,Cathy,Bobby,Judy,Eddie,Herbie,Bobby
+
+//people.splice(2,3,"Mat"); //Mary,Cathy,Mat,Herbie,Bobby
+
+
+//document.getElementById("peopleIKnow").innerHTML = people.valueOf();
